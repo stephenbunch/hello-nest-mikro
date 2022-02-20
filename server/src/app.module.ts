3906b0b58c9d8084cac1f8705cd31b7dbe4afaaf
Stephen Bunch <bunchclone@gmail.com>
2022-02-19 20:37:36 -0800

    Add static serving module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,19 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule, MikroOrmModule.forRoot()],
+  imports: [
+    TodoModule,
+    MikroOrmModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'web', 'build'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
