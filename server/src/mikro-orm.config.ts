80a319924104a10ec4f22435481e82bd59dbda4f
Stephen Bunch <bunchclone@gmail.com>
2022-02-19 22:41:21 -0800

    Try to fix db connection

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,23 +1,25 @@
 import { MikroOrmModuleOptions as Options } from '@mikro-orm/nestjs';
 import { Todo } from './entities/todo.entity';
-import { parse } from 'pg-connection-string';
 
 const url =
   process.env.DATABASE_URL ??
   'postgres://postgres:postgres@localhost:5432/postgres';
-const options = parse(url);
 
 const config: Options = {
   type: 'postgresql',
-  host: options.host,
-  port: options.port !== undefined ? parseInt(options.port, 10) : undefined,
-  user: options.user,
-  password: options.password,
-  dbName: options.database,
+  clientUrl: url,
   entities: [Todo],
   migrations: {
     path: 'dist/migrations',
     pathTs: 'src/migrations',
+  },
+  driverOptions: {
+    // https://github.com/mikro-orm/mikro-orm/issues/303#issuecomment-737342298
+    connection: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
 
